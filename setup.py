9eeb9f76a1add85a5df8821237fba06008d981ad
Jung Gyu Yoon <yjg30737@gmail.com>
2022-02-28 10:45:07 +0900

    Update version (1.3.1 -> 1.3.2)

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pyqt-custom-titlebar-window',
-    version='1.3.1',
+    version='1.3.2',
     author='Jung Gyu Yoon',
     author_email='yjg30737@gmail.com',
     license='MIT',
