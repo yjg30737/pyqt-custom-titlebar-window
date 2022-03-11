0e0e5285a6aa61a961d0cb4cc0b16f84909b16b3
Jung Gyu Yoon <yjg30737@gmail.com>
2022-03-12 07:56:44 +0900

    Update version (1.7.4 -> 1.7.5)

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pyqt-custom-titlebar-window',
-    version='1.7.4',
+    version='1.7.5',
     author='Jung Gyu Yoon',
     author_email='yjg30737@gmail.com',
     license='MIT',
