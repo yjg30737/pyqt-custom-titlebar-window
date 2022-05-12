516bc8b2cb0930720216f1c72a4aa869831ddac9
Jung Gyu Yoon <yjg30737@gmail.com>
2022-05-12 14:09:10 +0900

    Update setup.py to upload this to pypi

--- setup.py
+++ setup.py
@@ -1,14 +1,23 @@
 from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
 
 setup(
     name='pyqt-custom-titlebar-window',
-    version='1.7.7',
+    version='0.0.1',
     author='Jung Gyu Yoon',
     author_email='yjg30737@gmail.com',
     license='MIT',
     packages=find_packages(),
-    description='PyQt Custom Titlebar Window (resizable, movable, minimize/maximize/close)',
+    description='PyQt custom titlebar window (resizable, movable, minimize/maximize/close, etc.)',
     url='https://github.com/yjg30737/pyqt-custom-titlebar-window.git',
+    long_description_content_type='text/markdown',
+    long_description=long_description,
     install_requires=[
         'PyQt5>=5.15',
         'pyqt-frameless-window>=0.0.1',
