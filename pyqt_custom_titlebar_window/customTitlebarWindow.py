fa5d065dafa7527bee1820e09471f7c4635e5a2b
Jung Gyu Yoon <yjg30737@gmail.com>
2022-06-15 11:19:06 +0900

    Bug fix (QSizePolicy has not been included somehow)

--- pyqt_custom_titlebar_window/customTitlebarWindow.py
+++ pyqt_custom_titlebar_window/customTitlebarWindow.py
@@ -1,7 +1,7 @@
 from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QPalette, QFont, QIcon
 from PyQt5.QtWidgets import QHBoxLayout, QGridLayout, QWidget, QMainWindow, QPushButton, QLabel, \
-    QMenuBar, QToolButton, qApp
+    QMenuBar, QToolButton, qApp, QSizePolicy
 
 from pyqt_frameless_window.framelessWindow import FramelessWindow
 
