c834ed6535651d9ded31526186f6363e627319db
Jung Gyu Yoon <yjg30737@gmail.com>
2022-07-10 09:18:44 +0900

    Bump version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
 
 setup(
     name='pyqt-custom-titlebar-window',
-    version='0.0.38',
+    version='0.0.39',
     author='Jung Gyu Yoon',
     author_email='yjg30737@gmail.com',
     license='MIT',
